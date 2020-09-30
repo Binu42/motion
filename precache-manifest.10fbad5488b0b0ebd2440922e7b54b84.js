37c05e02d31aff0df06d5a646cecbcaca2b49126
Binu42 <kbinu42@gmail.com>
2020-09-30 13:37:12 +0530

    Updates

---
+++ precache-manifest.10fbad5488b0b0ebd2440922e7b54b84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3286c9a5cf8b9630a56f6f7c276e26",
+    "url": "/motion/index.html"
+  },
+  {
+    "revision": "f88efb010c5215f7e1fb",
+    "url": "/motion/static/css/main.51d2a2bb.chunk.css"
+  },
+  {
+    "revision": "dc240943c9ff912f738e",
+    "url": "/motion/static/js/2.84869453.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/motion/static/js/2.84869453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88efb010c5215f7e1fb",
+    "url": "/motion/static/js/main.cb5f7a00.chunk.js"
+  },
+  {
+    "revision": "3f788ba3a4f678bb31c5",
+    "url": "/motion/static/js/runtime-main.012c4a64.js"
+  },
+  {
+    "revision": "a9844bd6bfbc5e957f80824a7179ff53",
+    "url": "/motion/static/media/compressed-image2.a9844bd6.jpg"
+  },
+  {
+    "revision": "77193675142cb81357caaa58b76a9738",
+    "url": "/motion/static/media/model.77193675.jpg"
+  }
+]);
